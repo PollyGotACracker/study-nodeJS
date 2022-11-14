5aa65ddda4ea329a95a8503a4d055c25ebd83efa
PollyGotACracker <bitterns96@gmail.com>
2022-11-14 16:56:46 +0900

    erpDB Project

---
+++ Node_ERP/routes/buyer.js
@@ -0,0 +1,31 @@
+import express from "express";
+import DB from "../models/index.js";
+const Buyer = DB.models.tbl_buyer;
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const buyers = await Buyer.findAll();
+  res.render("buyer/list", { buyers });
+});
+
+router.get("/insert", (req, res) => {
+  res.render("buyer/write");
+});
+
+router.post("/insert", async (req, res) => {
+  const data = req.body;
+  console.log(data);
+  try {
+    await Buyer.create(data);
+    res.redirect("/buyer");
+  } catch (error) {
+    console.error(error);
+    res.send("SQL 오류");
+  }
+});
+
+router.get("/detail", (req, res) => {
+  res.render("buyer/detail");
+});
+
+export default router;
