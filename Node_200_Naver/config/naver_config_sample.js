071a5d2f88cb7c1f5c3f8a56bef1293bbfdfb579
PollyGotACracker <bitterns96@gmail.com>
2023-01-13 09:26:59 +0900

    naver config sample

---
+++ Node_200_Naver/config/naver_config_sample.js
@@ -0,0 +1,13 @@
+/**
+ * Naver 개발자 센터에서 발급받은 애플리케이션 Id Secret 을
+ * 다음 VALUE 항목에 추가한 후 이 파일을 naver_config.js 로 이름변경하여
+ * 프로젝트를 시작
+ */
+const CLIENT_ID = { KEY: "X-Naver-Client-Id", VALUE: "YOUR CLIENT ID" };
+const CLIENT_SECRET = {
+  KEY: "X-Naver-Client-Secret",
+  VALUE: "YOUR CLIENT SECRET",
+};
+const BOOK_URL_JSON = "https://openapi.naver.com/v1/search/blog.json";
+
+export { CLIENT_ID, CLIENT_SECRET, BOOK_URL_JSON };
